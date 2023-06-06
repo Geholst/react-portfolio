afe2fa24afd56c7994e7cb9d44638bceab692cdb
Geholst <geholst@msn.com>
2023-06-05 21:50:20 -0700

    updated resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,7 +2,7 @@ export default function Resume() {
     return ( 
         <section>
             <h1>Check out my Linkedin!</h1>
-            
+            <a id="Contact-btn" target="_blank" type="button" class="btn btn-dark" href="https://www.linkedin.com/in/george-holst-b813a41b0/" >Linkedin</a>
         </section>
     )
 }
