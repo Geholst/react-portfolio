86999e26ede225cf9bd7553a468bbce67eb7da55
Geholst <geholst@msn.com>
2023-06-05 23:03:03 -0700

    added projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,8 +1,45 @@
-
-export default function Projects () {
-    return (
-        <section> 
-            <h1>Check out my Projects!</h1>
+export default function Projects(){
+    const projects = [
+        {
+          name: 'Fast Card',
+          link: 'https://github.com/Geholst/Fast_Card',
+        },
+        {
+          name: 'Password Generator',
+          link: 'https://github.com/Geholst/Password-generator',
+        },
+        {
+          name: 'Weather dashboard',
+          link: 'https://github.com/Geholst/Weather-Dashboard',
+        },
+        {
+          name: 'Team Profile Generator',
+          link: 'https://github.com/Geholst/Team-Profile-Generator',
+        },
+        {
+          name: 'Social Network API',
+          link: 'https://github.com/Geholst/social-network-API',
+        },
+        {
+          name: 'README generator',
+          link: 'https://github.com/Geholst/README-Generator',
+        },
+      ];
+      
+    return(
+    <section>
+      <h1>My Projects</h1>
+        <section id="projects">
+            {projects.map((key, index)=>{
+            return(
+                <article key={index}>
+                <a href={key.link} target='_blank'>
+                {key.name}
+                </a>
+                </article>
+            );
+            })}
         </section>
-    )
+    </section>
+    );
 }
